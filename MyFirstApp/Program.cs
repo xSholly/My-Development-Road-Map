14df385b18c85e054c2ff6bac1159d738daaaeb4
xSholly <szymon.mista2012@gmail.com>
2025-04-08 17:41:42 +0200

    Lesson7

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {  
+        Console.Write("Give me one number: ");
+        string number = Console.ReadLine();
+        int parsednumber = int.Parse(number); 
+        Console.WriteLine($"your number,  {parsednumber}!");
+        Console.Write("Give me second number: ");
+        string number2 = Console.ReadLine();
+        int parsednumber2 = int.Parse(number2);
+        Console.WriteLine($"your number,  {parsednumber2}!");
+        Console.WriteLine($"Sume,  {parsednumber + parsednumber2}!");
+    }
+}
